286f27f93e2f20fdc8b51d8d153680321d4e0bfb
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-09-14 09:35:05 +0200

    Add bricks grid

---
+++ src/functions/Grid/Bricks.ts
@@ -0,0 +1,62 @@
+import { broadCast, PrimitiveFunction } from "@rkmodules/rules";
+import { IPoint, paths } from "makerjs";
+import { linesToCells } from "./liensToCells";
+
+export const bricks: PrimitiveFunction = {
+    name: "bricks",
+    label: "Bricks",
+    description: "Creates a bricks grid",
+    inputs: {},
+    params: {
+        width: "number",
+        height: "number",
+        nx: "number",
+        ny: "number",
+    },
+    outputs: {
+        p: "Point",
+        l: "Line",
+        m: "Model",
+    },
+    impl: async (inputs, params) => {
+        const points: IPoint[] = [];
+        const lines: paths.Line[] = [];
+
+        const nx = params.nx + 1;
+        const ny = params.ny * 2 + 1;
+        const hSpace = params.width / (nx - 1);
+        const vSpace = params.height / (ny - 1);
+        for (let j = 0; j < ny; j++) {
+            for (let i = 0; i < nx; i++) {
+                points.push([i * hSpace, j * vSpace]);
+
+                if (j % 2 === i % 2 && i < nx - 1) {
+                    // horizontal line
+                    lines.push(
+                        new paths.Line(
+                            [i * hSpace, j * vSpace],
+                            [(i + 1) * hSpace, j * vSpace]
+                        )
+                    );
+                }
+                if (j < ny - 1) {
+                    // vertical line
+                    lines.push(
+                        new paths.Line(
+                            [i * hSpace, j * vSpace],
+                            [i * hSpace, (j + 1) * vSpace]
+                        )
+                    );
+                }
+            }
+        }
+
+        const models = linesToCells(lines);
+
+        return {
+            p: broadCast(points),
+            l: broadCast(lines),
+            m: broadCast(models),
+        };
+    },
+};
--- src/functions/Grid/index.ts
+++ src/functions/Grid/index.ts
@@ -2,11 +2,13 @@ import { PrimitiveFunction } from "@rkmodules/rules";
 import { rectGrid } from "./RectGrid";
 import { triGrid } from "./TriGrid";
 import { hexGrid } from "./HexGrid";
+import { bricks } from "./Bricks";
 
 const primitives: Record<string, PrimitiveFunction> = {
     triGrid,
     rectGrid,
     hexGrid,
+    bricks,
 };
 
 export default primitives;

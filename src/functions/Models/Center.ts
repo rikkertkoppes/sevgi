85c46466f07b24c3b2720e38b5f64acaa7cd7dcd
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-09-23 14:15:23 +0200

    fix center, base on points

--- src/functions/Models/Center.ts
+++ src/functions/Models/Center.ts
@@ -1,5 +1,5 @@
 import { mapTree, PrimitiveFunction } from "@rkmodules/rules";
-import { measure } from "makerjs";
+import { chain, model } from "makerjs";
 
 export const center: PrimitiveFunction = {
     name: "center",
@@ -14,8 +14,17 @@ export const center: PrimitiveFunction = {
     impl: async (inputs) => {
         return {
             p: mapTree(inputs.m, (m) => {
-                const exts = measure.modelExtents(m);
-                return [exts.center];
+                const chains = model.findChains(m) as MakerJs.IChain[];
+                const points = chains.flatMap((c) => chain.toKeyPoints(c));
+                const center = points.reduce(
+                    (acc, p) => [
+                        acc[0] + p[0] / points.length,
+                        acc[1] + p[1] / points.length,
+                    ],
+                    [0, 0]
+                ) as MakerJs.IPoint;
+
+                return [center];
             }),
         };
     },

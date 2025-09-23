5b95fb073e7e6f8ae19f3d35145fb5d0d8d0dfc7
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-09-23 18:08:29 +0200

    fix mode component

--- src/functions/Models/Move.ts
+++ src/functions/Models/Move.ts
@@ -1,12 +1,12 @@
 import { binaryOnTree, PrimitiveFunction } from "@rkmodules/rules";
-import { model } from "makerjs";
+import { IModel, model } from "makerjs";
 
 export const move: PrimitiveFunction = {
     name: "move",
     label: "Move",
     description: "Move a model to an absolute poinl",
     inputs: {
-        o: "Point",
+        o: { type: "Point", default: [0, 0] },
         m: "Model",
     },
     outputs: {
@@ -17,9 +17,9 @@ export const move: PrimitiveFunction = {
             m: binaryOnTree(
                 inputs.o,
                 inputs.m,
-                (o, m) => {
+                (o, m: IModel) => {
                     m = model.clone(m);
-                    m.move(o);
+                    model.move(m, o);
                     return m;
                 },
                 true

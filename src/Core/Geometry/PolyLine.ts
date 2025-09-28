9fd871f60840f45bbc6af488f5b2e420bc386b3d
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-09-28 15:16:16 +0200

    improve polyline offset algorithm, adding chamfers and fillets

--- src/Core/Geometry/PolyLine.ts
+++ src/Core/Geometry/PolyLine.ts
@@ -1,17 +1,23 @@
+import { Arc } from "./Arc";
+import { Circle } from "./Circle";
 import { Curve } from "./Curve";
-import { LineSegment } from "./Line";
-import { Point } from "./Vector";
+import { Line, LineSegment } from "./Line";
+import { Segment } from "./Segment";
+import { Point, same } from "./Vector";
 
+type JoinType = "miter" | "bevel" | "round" | "none";
 export class PolyLine extends Curve {
     public length: number;
     private lengths: number[] = [];
     private offsets: number[] = [];
-    private edges: LineSegment[] = [];
-    constructor(public lines: LineSegment[]) {
+    private segments: Segment[] = [];
+    private closed = false;
+    private joined = false;
+    constructor(segments: Segment[]) {
         super();
         // TODO: order
-        this.edges = lines;
-        this.lengths = this.edges.map((e) => e.length);
+        this.segments = segments;
+        this.lengths = this.segments.map((e) => e.length);
         this.offsets = this.lengths.reduce(
             (acc, l) => {
                 const last = acc.length > 0 ? acc[acc.length - 1] : 0;
@@ -21,19 +27,25 @@ export class PolyLine extends Curve {
             [0] as number[]
         );
         this.length = this.lengths.reduce((sum, l) => sum + l, 0);
+        const jointsClosed = this.segments.map((s, i) => {
+            const next = this.segments[(i + 1) % this.segments.length];
+            return same(s.end, next.start);
+        });
+        this.joined = jointsClosed.slice(0, -1).every((v) => v); // don't care about last to first joint
+        this.closed = jointsClosed.every((v) => v);
     }
 
     public clone(): PolyLine {
-        return new PolyLine(this.lines.map((p) => p.clone()));
+        return new PolyLine(this.segments.map((p) => p.clone()));
     }
     public translate(v: Point): PolyLine {
-        return new PolyLine(this.lines.map((p) => p.translate(v)));
+        return new PolyLine(this.segments.map((p) => p.translate(v)));
     }
     public rotate(angle: number, center: Point): PolyLine {
-        return new PolyLine(this.lines.map((p) => p.rotate(angle, center)));
+        return new PolyLine(this.segments.map((p) => p.rotate(angle, center)));
     }
     public scale(factor: number, center: Point): PolyLine {
-        return new PolyLine(this.lines.map((p) => p.scale(factor, center)));
+        return new PolyLine(this.segments.map((p) => p.scale(factor, center)));
     }
 
     private getLocalT(globalT: number) {
@@ -53,29 +65,29 @@ export class PolyLine extends Curve {
 
     public pointAt(t: number): Point {
         const { edgeIndex, t: localT } = this.getLocalT(t);
-        return this.edges[edgeIndex].pointAt(localT);
+        return this.segments[edgeIndex].pointAt(localT);
     }
     public normalAt(t: number): Point {
         const { edgeIndex, t: localT } = this.getLocalT(t);
-        return this.edges[edgeIndex].normalAt(localT);
+        return this.segments[edgeIndex].normalAt(localT);
     }
     public tangentAt(t: number): Point {
         const { edgeIndex, t: localT } = this.getLocalT(t);
-        return this.edges[edgeIndex].tangentAt(localT);
+        return this.segments[edgeIndex].tangentAt(localT);
     }
     public offsetAt(t: number, distance: number): Point {
         const { edgeIndex, t: localT } = this.getLocalT(t);
-        return this.edges[edgeIndex].offsetAt(localT, distance);
+        return this.segments[edgeIndex].offsetAt(localT, distance);
     }
     public curvatureAt(t: number): number {
         const { edgeIndex, t: localT } = this.getLocalT(t);
-        return this.edges[edgeIndex].curvatureAt(localT);
+        return this.segments[edgeIndex].curvatureAt(localT);
     }
     public findClosestPoint(p: Point) {
         let closest: { t: number; point: Point; distance: number } | null =
             null;
         let accLength = 0;
-        this.edges.forEach((edge, i) => {
+        this.segments.forEach((edge, i) => {
             const cp = edge.findClosestPoint(p);
             if (!closest || cp.distance < closest.distance) {
                 closest = {
@@ -90,16 +102,102 @@ export class PolyLine extends Curve {
     }
 
     public getSegments() {
-        return this.edges;
+        return this.segments;
     }
 
-    public offset(d: number): PolyLine {
-        return new PolyLine(this.lines.map((p) => p.offset(d)));
+    public offset(d: number, joinType: JoinType = "round"): PolyLine {
+        if (this.segments.length === 0) return new PolyLine([]);
+
+        // 1) Offset each edge individually
+        const raw = this.segments.map((e) => e.offset(d));
+
+        // 2) Stitch with trimming or chamfering
+        const result: Segment[] = [];
+
+        // Start with a copy of the first offset edge
+        let curr = raw[0];
+        const lastToVisit = this.closed ? raw.length : raw.length - 1;
+
+        for (let i = 0; i < lastToVisit; i++) {
+            const next = raw[(i + 1) % raw.length];
+            const endLine = Line.fromPointAndDirection(
+                curr.end,
+                curr.tangentAt(1)
+            );
+            const startLine = Line.fromPointAndDirection(
+                next.start,
+                next.tangentAt(0)
+            );
+
+            const joints = curr.intersectWith(next);
+
+            if (joints.length) {
+                // (A) They meet (or cross) within both segments -> trim to intersection
+                // Commit the trimmed prev; move on with trimmed curr
+
+                // TODO find the one closest to the original joint?
+
+                curr.end = joints[0];
+                next.start = joints[0];
+                result.push(curr);
+            } else {
+                switch (joinType) {
+                    case "miter": {
+                        const p = Line.intersection(endLine, startLine);
+                        if (p) {
+                            // Commit the extended prev; move on with extended curr
+                            curr.end = p;
+                            next.start = p;
+                            result.push(curr);
+                        } else {
+                            // parallel lines - just join them
+                            result.push(curr);
+                        }
+                        break;
+                    }
+                    case "bevel": {
+                        // create chamfer
+                        const chamfer = new LineSegment(curr.end, next.start);
+
+                        result.push(curr);
+                        result.push(chamfer);
+                        break;
+                    }
+                    case "round": {
+                        // create rounded corner fillet
+                        const dir = Math.sign(d);
+                        const c = this.segments[i].end;
+                        const arc = new Arc(
+                            new Circle(c, d),
+                            endLine.angle() - (dir * Math.PI) / 2,
+                            startLine.angle() - (dir * Math.PI) / 2
+                        );
+                        result.push(curr);
+                        result.push(arc);
+                        break;
+                    }
+                    case "none": {
+                        // leave gaps
+                        result.push(curr);
+                        break;
+                    }
+                }
+            }
+
+            curr = next;
+        }
+
+        // Push the last carried segment, unless the poly is closed, in which case this segment is the first and was already pushed
+        if (!this.closed) {
+            result.push(curr);
+        }
+
+        return new PolyLine(result);
     }
 
     public getPoints() {
         const index: Record<string, Point> = {};
-        this.edges.forEach((e) => {
+        this.segments.forEach((e) => {
             index[e.start.hash()] = e.start;
             index[e.end.hash()] = e.end;
         });
@@ -107,10 +205,15 @@ export class PolyLine extends Curve {
     }
 
     public toSVG() {
-        return this.edges.map((l) => l.toSVG()).join(" ");
+        if (!this.joined) {
+            return this.segments.map((l) => l.toSVG()).join(" ");
+        }
 
-        const [first, ...rest] = this.edges;
+        const [first, ...rest] = this.segments;
         const path = first.toSVG() + rest.map((l) => l.toSVGRel()).join(" ");
+        if (this.closed) {
+            return path + " z";
+        }
 
         return path;
     }

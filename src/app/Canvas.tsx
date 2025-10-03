b9e1c80425838e96e3aef5257eedfded863c348b
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-10-03 12:14:05 +0200

    improve canvas zoom

--- src/app/Canvas.tsx
+++ src/app/Canvas.tsx
@@ -18,25 +18,47 @@ const Geometry = React.memo(({ d, c }: GeometryProps) => {
 });
 Geometry.displayName = "Geometry";
 
+function getSvgPoint(container: SVGSVGElement, x: number, y: number) {
+    const pt = container.createSVGPoint();
+    pt.x = x;
+    pt.y = y;
+    const ctm = container.getScreenCTM();
+    if (ctm) {
+        const p = pt.matrixTransform(ctm.inverse());
+        return { x: p.x, y: p.y };
+    }
+}
+
 interface SVGScrollerProps {
     children?: React.ReactNode;
 }
 function SVGScroller({ children }: SVGScrollerProps) {
+    const svgRef = React.useRef<SVGSVGElement>(null);
     const [offset, setOffset] = React.useState({ x: 0, y: 0 });
     const [zoom, setZoom] = React.useState(1);
 
     const bind = useGesture({
-        onDrag: ({ offset: [ox, oy] }) => {
-            setOffset({ x: ox, y: oy });
+        onDrag: ({ delta: [dx, dy] }) => {
+            setOffset(({ x, y }) => ({ x: x + dx, y: y + dy }));
         },
-        onWheel: ({ delta: [, dy] }) => {
-            setZoom((z) => Math.max(0.1, z - dy * 0.001 * z));
+        onWheel: ({ delta: [, dy], event }) => {
+            const svg = svgRef.current;
+            if (!svg) return;
+            const pt = getSvgPoint(svg, event.pageX, event.pageY);
+            if (!pt) return;
+            const newZoom = Math.max(0.1, zoom - dy * 0.001 * zoom);
+            // adjust offset to zoom arount mouse position
+            setOffset((offset) => ({
+                x: (newZoom * (offset.x - pt.x)) / zoom + pt.x,
+                y: (newZoom * (offset.y - pt.y)) / zoom + pt.y,
+            }));
+            setZoom(newZoom);
         },
     });
 
     return (
         <div className={styles.Scroll} {...bind()}>
-            <svg>
+            <svg ref={svgRef}>
                 <defs>
                     <marker
                         id="cross"

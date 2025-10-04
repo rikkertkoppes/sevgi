bf59736ad894627f4fc91763499c6c1cdda5b7c5
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-10-04 09:26:45 +0200

    Work on landing page, added rudimentary getting started section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,55 @@ export default function Home() {
                     </ul>
                 </div>
             </div>
+            <div className={styles.Start}>
+                <h1>Getting Started</h1>
+                <p>
+                    Sevgi is different. Rather than drawing on a canvas, you
+                    define shapes using a <i>graph</i> of <i>nodes</i>. A graph
+                    is a network of elements with connections between them. We
+                    call those elements <i>nodes</i>.
+                </p>
+                <p>
+                    <img src="/img/circle.png" alt="Circle Node" />
+                </p>
+                <p>
+                    Above is a `Circle` node connected to the output. It draws a
+                    circle. Simple right?
+                </p>
+                <p>
+                    By connecting a `Point` node to the circle node, we can
+                    change its center point:
+                </p>
+                <p>
+                    <img
+                        src="/img/point-circle.png"
+                        alt="Circle Node with Point input"
+                    />
+                </p>
+                <p>
+                    However, by connecting a grid of points to the circle, we
+                    instantly get a whole grid of circles. This gets powerful{" "}
+                    <i>very fast</i>.
+                </p>
+                <p>
+                    <img
+                        src="/img/trigrid-circle.png"
+                        alt="Circle Node with Grid input"
+                    />
+                </p>
+                <p>
+                    By using the `Preview` panel of a node, you can have a look
+                    at what it outputs. Click the output parameter to change the
+                    view. Also, when a node is selected, the first output
+                    parameter is highlighted on the canvas:
+                </p>
+                <p>
+                    <img
+                        src="/img/trigrid-circle-preview.png"
+                        alt="Circle Node with Grid input and preview panels open"
+                    />
+                </p>
+            </div>
         </div>
     );
 }

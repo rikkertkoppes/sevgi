ffc72f149d596e5de8b33d46b420e68c071fe22b
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-10-06 23:59:54 +0200

    Additions to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,7 +115,61 @@ export default function Home() {
                         alt="Circle Node with Grid input and preview panels open"
                     />
                 </p>
-                {/* <h1>Things to Draw</h1> */}
+                <h1>What can I do with...</h1>
+                <h2>Points</h2>
+                <p>
+                    Points can be created using the Point node. Lots of other
+                    nodes use points as input. For example a line has a start
+                    and end point, a polyline can be constructed from points and
+                    a circle has a center point. Other ways to create points is
+                    to use a pattern or to get points from a line or a polyline.
+                    In those latter cases, you get a list of points, which you
+                    can use to create multiple instances of things.
+                </p>
+                <p>
+                    For example, take a look at this setup, where the centers of
+                    hexagons are used to center a whole lot of circles. Try
+                    connecting the points output of the hexgrid to the circle
+                    instead and see what changes. Alternatively, use a point
+                    input for the cirle. Look for orangey functions and outputs.
+                </p>
+                <p>
+                    <img
+                        src="/img/hexgrid-center-circle.png"
+                        alt="Hexgrid of circles"
+                    />
+                </p>
+                <h2>Patterns</h2>
+                <p>
+                    Patterns can be a nice start for some tesselation or pattern
+                    designs. There are a few options available. You can use the
+                    shapes directly, or use the lines or points outputs to place
+                    other things on a grid.
+                </p>
+                <p>
+                    Patterns play very nice with the `star pattern` function and
+                    the `outline` function. Use a negative outline (inset) to
+                    create a nice pattern that can be laser cut:
+                </p>
+                <p>
+                    <img
+                        src="/img/hexgrid-star-outline.png"
+                        alt="Star pattern in a hexgrid"
+                    />
+                </p>
+                <h2>Math</h2>
+                <p>
+                    Use math to calculate things. This is especially useful if
+                    you use for example a coordinate of a center to drive an
+                    input of another function. You can use this to create
+                    variations:
+                </p>
+                <p>
+                    <img
+                        src="/img/sqgrid-destruct-calc-circle.png"
+                        alt="Star pattern in a hexgrid"
+                    />
+                </p>
             </div>
         </div>
     );

3c771b183c000521dee752e4f68ca62896d1a3fe
rikkertkoppes <rikkert@rikkertkoppes.com>
2025-10-08 10:28:59 +0200

    extract scrollZoom component

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
 import { toArray, Tree } from "@rkmodules/rules";
-import { useGesture } from "@use-gesture/react";
 
 import { BaseGeometry } from "@/Core/Geometry/BaseGeometry";
 import { Point } from "@/Core/Geometry/Vector";
+import { SVGScroller } from "./ScrollZoom";
 
 import "@rkmodules/rules/index.css";
 import styles from "./Canvas.module.css";
@@ -18,86 +18,6 @@ const Geometry = React.memo(({ d, c }: GeometryProps) => {
     return <path d={d} className={c} />;
 });
 Geometry.displayName = "Geometry";
-
-function getSvgPoint(container: SVGSVGElement, x: number, y: number) {
-    const pt = container.createSVGPoint();
-    pt.x = x;
-    pt.y = y;
-    const ctm = container.getScreenCTM();
-    if (ctm) {
-        const p = pt.matrixTransform(ctm.inverse());
-        return { x: p.x, y: p.y };
-    }
-}
-
-interface SVGScrollerProps {
-    children?: React.ReactNode;
-}
-function SVGScroller({ children }: SVGScrollerProps) {
-    const svgRef = React.useRef<SVGSVGElement>(null);
-    const [offset, setOffset] = React.useState({ x: 0, y: 0 });
-    const [zoom, setZoom] = React.useState(1);
-
-    const bind = useGesture({
-        onDrag: ({ delta: [dx, dy] }) => {
-            setOffset(({ x, y }) => ({ x: x + dx, y: y + dy }));
-        },
-        onWheel: ({ delta: [, dy], event }) => {
-            const svg = svgRef.current;
-            if (!svg) return;
-            const pt = getSvgPoint(svg, event.pageX, event.pageY);
-            if (!pt) return;
-            const newZoom = Math.max(0.1, zoom - dy * 0.001 * zoom);
-            // adjust offset to zoom arount mouse position
-            setOffset((offset) => ({
-                x: (newZoom * (offset.x - pt.x)) / zoom + pt.x,
-                y: (newZoom * (offset.y - pt.y)) / zoom + pt.y,
-            }));
-            setZoom(newZoom);
-        },
-        onPinch: ({ origin: [ox, oy], offset: [s], event }) => {
-            const svg = svgRef.current;
-            if (!svg) return;
-            const pt = getSvgPoint(svg, ox, oy);
-            if (!pt) return;
-            const newZoom = Math.max(0.1, s);
-            // adjust offset to zoom arount pinch origin
-            setOffset((offset) => ({
-                x: (newZoom * (offset.x - pt.x)) / zoom + pt.x,
-                y: (newZoom * (offset.y - pt.y)) / zoom + pt.y,
-            }));
-            setZoom(newZoom);
-            event.preventDefault();
-        },
-    });
-
-    return (
-        <div className={styles.Scroll} {...bind()}>
-            <svg ref={svgRef}>
-                <defs>
-                    <marker
-                        id="cross"
-                        viewBox="-10 -10 20 20"
-                        refX="0"
-                        refY="0"
-                        markerWidth="8"
-                        markerHeight="8"
-                        orient="auto-start-reverse"
-                    >
-                        <path d="M -10 -10 L 10 10 M -10 10 L 10 -10 z" />
-                    </marker>
-                </defs>
-                <g
-                    transform={`translate(${offset.x},${
-                        offset.y
-                    }) scale(${zoom},${-zoom})`}
-                >
-                    {children}
-                </g>
-            </svg>
-        </div>
-    );
-}
 
 function serializeSVG(geometry: BaseGeometry[]) {
     // todo: calculate proper viewBox

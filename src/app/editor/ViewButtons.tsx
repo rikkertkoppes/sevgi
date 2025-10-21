a7531f67ef8734b075bb8aedaceaf84d7d612393
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-10-21 16:05:06 +0200

    Add fullscreen toggle

--- src/app/editor/ViewButtons.tsx
+++ src/app/editor/ViewButtons.tsx
@@ -6,20 +6,41 @@ import styles from "./editor.module.css";
 
 export default function ViewButtons() {
     const [flags, setFlags] = React.useState(getClientCookieFlags());
+    const [fs, setFs] = React.useState<boolean>(false);
     const toggleVertical = () => {
         toggleCookieFlag("verticalView");
         setFlags(getClientCookieFlags());
     };
-    const symbol = flags.verticalView ? "viewHorizontal" : "viewVertical";
+    const toggleFullScreen = () => {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+            setFs(true);
+        } else if (document.exitFullscreen) {
+            document.exitFullscreen();
+            setFs(false);
+        }
+    };
+    const orSymbol = flags.verticalView ? "viewHorizontal" : "viewVertical";
+    const fsSymbol = fs ? "fullscreenExit" : "fullscreenEnter";
 
     return (
         <div className={styles.ViewButtons}>
             <button
+                className={styles.Orientation}
                 onClick={toggleVertical}
                 title="Toggle viewport orientation"
             >
                 <svg>
-                    <use href={`/symbols.svg#${symbol}`}></use>
+                    <use href={`/symbols.svg#${orSymbol}`}></use>
+                </svg>
+            </button>
+            <button
+                className={styles.Fullscreen}
+                onClick={toggleFullScreen}
+                title="Toggle fullscreen"
+            >
+                <svg>
+                    <use href={`/symbols.svg#${fsSymbol}`}></use>
                 </svg>
             </button>
         </div>

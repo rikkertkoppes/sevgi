6afb291a2772b9019377c125d67d76afe72ad5d7
Rikkert Koppes <rikkert@rikkertkoppes.com>
2025-10-22 11:20:56 +0200

    Add flip param to 4.8.8 grid, also rename

--- src/functions/Grid/TruncatedSquareGrid.ts
+++ src/functions/Grid/TruncatedSquareGrid.ts
@@ -1,11 +1,15 @@
 import { broadCast, PrimitiveFunction } from "@rkmodules/rules";
-import { linesToCells } from "./linesToCells";
-import { Point, rot, sum, v2 } from "@/Core/Geometry/Vector";
-import { LineSegment } from "@/Core/Geometry/LineSegment";
+import { diff, Point, v2 } from "@/Core/Geometry/Vector";
+import { RegularPolygon } from "@/Core/Geometry/RegularPolygon";
+import { Transform } from "@/Core/Geometry/Transform";
+
+const baseOct = RegularPolygon.from(Point.ZERO, 8, 1, Math.PI / 8, false);
+const baseQuad = RegularPolygon.from(Point.ZERO, 4, 1, 0, false);
+const QPI = Math.PI / 4;
 
 export const truncatedSquareGrid: PrimitiveFunction = {
     name: "truncatedSquareGrid",
-    label: "Truncated Square Grid",
+    label: "4.8.8 Grid",
     description:
         "Creates a truncated square grid made up of octagons and squares",
     inputs: {},
@@ -13,6 +17,7 @@ export const truncatedSquareGrid: PrimitiveFunction = {
         size: { type: "number", default: 10 },
         nx: { type: "number", default: 5 },
         ny: { type: "number", default: 5 },
+        flip: { type: "boolean", default: false },
     },
     outputs: {
         shapes: "PolyLine",
@@ -20,53 +25,68 @@ export const truncatedSquareGrid: PrimitiveFunction = {
         points: "Point",
     },
     impl: async (inputs, params) => {
-        const pointsMap: Record<string, Point> = {};
-
-        function getUnique(p: Point) {
-            const h = p.hash;
-            if (pointsMap[h]) {
-                return pointsMap[h];
-            } else {
-                pointsMap[h] = p;
-                return p;
-            }
-        }
-
-        const points: Point[] = [];
-        const lines: LineSegment[] = [];
-
+        const f = params.flip;
         const nx = params.nx;
         const ny = params.ny;
-        const s = params.size;
-        const sl = s / (1 + Math.sqrt(2));
+        let s = params.size;
+        const rOct = s / 2;
+        const rQuad = Math.sqrt(2) * rOct - rOct;
+        const shapes: RegularPolygon[] = [];
+        const aOct = Math.PI / 8;
+        const halfOffset = v2(rOct, rOct);
+        if (f) s += 2 * rQuad;
         for (let i = 0; i < nx; i++) {
             for (let j = 0; j < ny; j++) {
-                const pts: Point[] = [];
-                let p = getUnique(v2(i * s, j * s));
-                pts.push(p);
-                let v = v2(sl, 0);
-                // create the octagon
-                for (let k = 1; k < 8; k++) {
-                    p = getUnique(sum(p, v));
-                    pts.push(p);
-                    v = rot(Math.PI / 4, v);
+                const v = v2(i * s, j * s);
+                const transform = Transform.from(v, aOct, rOct);
+                const oct = baseOct.transform(transform).makeUnique();
+                shapes.push(oct);
+                if (j > 0 && i > 0) {
+                    if (f) {
+                        // create oct
+                        const vo = diff(v, v2(s / 2, s / 2));
+                        const transform = Transform.from(vo, aOct, rOct);
+                        const oct = baseOct.transform(transform).makeUnique();
+                        shapes.push(oct);
+                    } else {
+                        // create quad
+                        const vq = diff(v, halfOffset);
+                        const transform = Transform.from(vq, 0, rQuad);
+                        const quad = baseQuad.transform(transform).makeUnique();
+                        shapes.push(quad);
+                    }
                 }
-                points.push(...pts);
-                lines.push(
-                    ...pts.map((p1, i) => {
-                        const p2 = pts[(i + 1) % pts.length];
-                        return new LineSegment(p1, p2);
-                    })
-                );
+                if (f && i > 0) {
+                    // create quad
+                    const vq = diff(v, v2(s / 2, 0));
+                    const transform = Transform.from(vq, QPI, rQuad);
+                    const quad = baseQuad.transform(transform).makeUnique();
+                    shapes.push(quad);
+                }
+                if (f && j > 0) {
+                    // create quad
+                    const vq = diff(v, v2(0, s / 2));
+                    const transform = Transform.from(vq, QPI, rQuad);
+                    const quad = baseQuad.transform(transform).makeUnique();
+                    shapes.push(quad);
+                }
             }
         }
 
-        const models = linesToCells(lines);
+        const segments = shapes.flatMap((s) => s.getSegments());
+        const uniqueSegments = Array.from(
+            new Map(segments.map((s) => [s.hash, s])).values()
+        );
+
+        const points = uniqueSegments.flatMap((s) => [s.start, s.end]);
+        const uniquePoints = Array.from(
+            new Map(points.map((p) => [p.hash, p])).values()
+        );
 
         return {
-            shapes: broadCast(models),
-            lines: broadCast(lines),
-            points: broadCast(points),
+            shapes: broadCast(shapes),
+            lines: broadCast(uniqueSegments),
+            points: broadCast(uniquePoints),
         };
     },
 };
